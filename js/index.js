1e1a34465ffa294a8f5d15815019bd8fcf2f7763
SLewson <slewson@calpoly.edu>
2015-02-19 17:12:03 -0800

    moved js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
 var myAlarms;
 
+function setupFacebook() {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1395985730713299',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+    FB.getLoginStatus(function(response) {
+      console.log("getLoginStatus")
+      if (response.status === 'connected') {
+        console.log('Logged in.');
+        getAllAlarms()
+      }
+      else {
+        FB.login();
+      }
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "http://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+}
+
 function showAlarmPopup() {
   $("#mask").removeClass("hide");
   $("#popup").removeClass("hide");
@@ -140,7 +168,7 @@ function getTemp() {
 }
 
 $(document).ready(function() {
+  setupFacebook()
   getTime()
   getTemp()
-  getAllAlarms()
 });
